3594eaaf3b3e1e86b4e43a7bf533b375b9e4f9c6
HitHat02 <113663686+HitHat02@users.noreply.github.com>
2025-05-22 14:37:10 +0900

    main.py 수정 및 README.md 기입진행

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ def gpr_processing(rd3):
 
 path = "C:\\Users\\HYH\\Desktop\\AI_분석\\01 RAWDATA\\SBR_013\\00\\"
 filename = "SBR_013.rd3"
+start_idx = 100
+length = 200
+save_path = "C:\\Users\\HYH\\Desktop\\AI_분석\\01 RAWDATA\\SBR_013\\00"
 
 # 파일 읽고 데이터 정렬 진행
 rd3 = readRd3(path, filename)
 rd3 = gpr_processing(rd3)
 
 # 원하는 크기로 이미지를 자름
-start_idx = 100
-length = 200
 rd3 = cutRd3(rd3, start_idx, length)
 
 # 이미지를 저장하는 프로세스 (추후 프런트와 연동하여 원하는 이미지 모양(크기)로 저장하는 모듈 개발 예정)
-image_save(rd3, "C:\\Users\\HYH\\Desktop\\AI_분석\\01 RAWDATA\\SBR_013\\00\\")
+image_save(rd3, save_path)
 
 plot_gpr_image(rd3, channel=9)
 
